a13976a356567b8eccb6910196938a0e5aed96e2
Mitchel Parish <mitchelparish@gmail.com>
2019-01-02 17:22:52 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,9 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  my_hash = {
+    "rocket" = "Saturn V", 
+    "capsule" = "Command Module",
+    "lander" = "Lunar Excursion Module"
+  }
 
   
 end
